4904cc604f7b4be1686e39f06a16530836002d18
Michel Frechette <mifrec@hotmail.com>
2016-03-05 19:08:26 -0500

    CS Python Level One

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 > Learn to code in the comfort of your browser with video tutorials, programming challenges, and screencasts.
 
 > Free Weekend is 55 hours long, starting on Friday, March 4, at 5:00 p.m. EST and ending on Sunday, March 6, at 11:59 p.m. EST. 2016
+
+[python style guide](http://legacy.python.org/dev/peps/pep-0008/)
+<br>
+[python docs](https://www.codeschool.com/pluralsight-courses/python-fundamentals)
--- index.py
+++ index.py
@@ -1,4 +1,22 @@
 # hello world
+# python math
 print "hello world"
 print 2016 - 1964
 print (200 - 2) * 0.65 - 100
+# python variables
+# a person can carry 1/3 of its weight safely
+# percentage = 1/3
+# weight_to_be_carried = 50
+# # a person can weight 150 pounds
+# weight_of_a_person = 150
+# # therefore,the weight * the percentage will equal the weight that person can carry.
+# weight_that_a_person_can_carry = weight * percentage
+# print weight_that_can_be_carried
+import math
+perc = 1/3.0
+coco_wt = 1450
+macaw_wt = 900
+macaw_limit = macaw_wt * perc
+
+num_macaw = coco_wt/macaw_limit
+print math.ceil(num_macaw)
